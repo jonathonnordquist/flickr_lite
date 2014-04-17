a2970de778e0bdb46b09522f7b7804f39308bf8c
Jonathon <jonathon.nordquist@gmail.com>
2014-04-17 15:02:34 -0500

    Have working slide in and form showing.  Using absolute positioning on the container div.

--- public/js/image_manip.js
+++ public/js/image_manip.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   $(".thumb_nail").click(function(){
     event.preventDefault();
     console.log(this)
+    $(this).wrap(function(){
+      return "<div class='current'>";
+    });
     $(this).switchClass("thumb_nail", "current_image", 2000, "easeInQuad");
     $(this).removeClass("thumb_nail");
-    $(this).wrap(function(){
-      return "<div class='current'"> + this + "</div>";
-    });
+    appendForm();
   });
 });
+
+appendForm = function(){
+  $(".current").append(
+    '<form method="post" action=""><textarea id="comment_content" rows="6" cols="50" placeholder="Leave a comment..."></textarea></br><input type="hidden" id="users_id" value="<%= session[:user_id] %>"><input type="hidden" id="image_id" value="<%= @image.id %>"><input type="submit" value="Post Comment" id="post_comment"></form><div id="posted_comments"></div>'
+    )
+};
